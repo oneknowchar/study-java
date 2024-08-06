45fe884db515e29001d7792a17a291c52fda9d03
oneknowchar <oneknowchar@gmail.com>
2024-08-06 23:35:14 +0900

    Array

---
+++ src/book/Array.java
@@ -0,0 +1,54 @@
+package book;
+
+public class Array {
+	public static void main(String[] args) {
+		int [] aaa = {1,2,3,4};
+		int [] bbb= {1,2,3,4,};
+		System.out.println(aaa[1] == bbb[1]);
+		System.out.println("-------------------------------------------------------------");
+		
+		int[][] arr = {
+				{ 5, 5, 5, 5, 5},
+				{ 10, 10, 10},
+				{ 20, 20, 20, 20},
+				{ 30, 30} // <<< 이놈길이
+		};
+		System.out.println(arr[3].length);
+		System.out.println("-------------------------------------------------------------");
+
+		int[]arrr = {10,20,30,40,50};
+		int sum = 0;
+		for(int a : arrr) {
+			sum += a;
+		}
+		System.out.println("sum = " + sum);
+		System.out.println("-------------------------------------------------------------");
+		
+		//2차원 배열에 담긴 모든 값의 총 합과 평균을 구하는 프로그램
+		int[][] score = {
+				{ 5, 5, 5, 5, 5},
+				{10,10,10,10,10},
+				{20,20,20,20,20},
+				{30,30,30,30,30}
+		};
+		
+		int total = 0;
+		float average = 0;
+		
+		int totalCnt = 0;
+		for(int i = 0; i < score.length; i++) {
+			totalCnt += score[i].length;
+			for(int j = 0; j < score[i].length; j++){
+				total+= score[i][j];
+			}
+		}
+		
+		average = (float)total / totalCnt;
+		
+		System.out.println("total="+total); // 결과 : total=325
+		System.out.println("average="+average); // 결과 : average=16.25
+		System.out.println("-------------------------------------------------------------");
+		
+		
+	}
+}
