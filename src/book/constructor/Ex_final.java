fc2ba2cbacb2075be6d3a7cf697ea1a2705da278
oneknowchar <oneknowchar@gmail.com>
2024-08-18 21:04:24 +0900

    final 필드 선언시 또는 생성자에 초기화

---
+++ src/book/constructor/Ex_final.java
@@ -0,0 +1,35 @@
+package book.constructor;
+
+public class Ex_final {
+	public static void main(String[] args) {
+		Card c = new Card();
+		
+
+		//c.KIND = "dfas";	//에러 발생! final 선언 후 수정 불가
+		System.out.println(c);
+
+	}
+}
+
+class Card{
+	final int NUMBER;
+	final String KIND;
+	
+	static int width = 100;
+	static int height = 250;
+	
+	Card(){
+		this("HEART", 1);
+	}
+	
+	Card(String kind, int num){
+		KIND = kind;
+		NUMBER = num;
+	}
+	
+	@Override
+	public String toString() {
+		return "Card [NUMBER=" + NUMBER + ", KIND=" + KIND + "]";
+	}
+
+}
