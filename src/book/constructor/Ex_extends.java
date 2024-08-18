37a2bcdf7447ced054bea55f752179da4bf51789
oneknowchar <oneknowchar@gmail.com>
2024-08-18 21:14:29 +0900

    extends 시 메서드는 '오버라이딩', 필드는 '참조타입'을 따라감

---
+++ src/book/constructor/Ex_extends.java
@@ -0,0 +1,30 @@
+package book.constructor;
+
+public class Ex_extends {
+	public static void main(String[] args) {
+		Parent p = new Child();
+		Child child = new Child();
+		
+		System.out.println(p.x);		//100
+		System.out.println(child.x);	//200
+		
+		p.method();						//Child........
+		child.method();					//Child........
+	}
+}
+
+class Parent {
+	int x = 100;
+	
+	void method() {
+		System.out.println("parent........");
+	}
+}
+
+class Child extends Parent{
+	int x = 200;
+	
+	void method() {
+		System.out.println("Child........");
+	}
+}
